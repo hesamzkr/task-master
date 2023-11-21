729081039502e3ccb858bddb77689e8a98630e0f
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-11-21 17:13:34 +0100

    add navigation layout

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -5,12 +5,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Task Master</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="//unpkg.com/alpinejs" defer></script>
+
 </head>
 
 <body class="">
-  <x-site-layout-navbar />
+
 </body>
 
 </html>
