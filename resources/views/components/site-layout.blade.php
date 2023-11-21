5754f666d5ff3e728a024d377959c673db0e6955
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-11-21 14:09:40 +0100

    add laravel breeze

---
+++ resources/views/components/site-layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Task Master</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+
+<body class="">
+  <x-site-layout-navbar />
+</body>
+
+</html>
