536501669aad75449f36673d32c21391abf31b84
Hesam Zakeri <hesam.zk.81@gmail.com>
2023-11-23 23:41:09 +0100

    add caching to welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class WelcomeController extends Controller
 {
     public function welcome(Request $request)
     {
-        return view('welcome');
+        $cacheKey = 'welcome_page';
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
+        $content = view('welcome')->render();
+
+        Cache::put($cacheKey, $content, now()->addHour());
+
+        return $content;
     }
 }
